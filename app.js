2396a5af2aec29e53f1f00f56753d1b6da8a166d
joshraiborde <joshraiborde@gmail.com>
2022-02-26 14:13:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request")
+
+const app = express();
+const port = 3000;
+var now = new Date();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+
+    console.log("Server is running on Port " + port + " on " + now.toUTCString());
+})
+
+app.listen(port, () => {
+    console.log("Server is running on Port " + port + " on " + now.toUTCString());
+})
